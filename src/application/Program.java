5a66a11373cbe4d0bfefea85ad67b22a6a044799
rodsilca <cabralrodrigo787@gmail.com>
2025-07-31 18:00:54 -0400

    Exceptions and cleaning screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
 import boardgame.Board;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
@@ -13,16 +15,22 @@ public class Program {
         Scanner sc = new Scanner(System.in);
 
         while (true){
-            UI.printBoard(match.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
+            try {
+                UI.clearScreen();
+                UI.printBoard(match.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
 
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
 
-            ChessPiece capturedPiece = match.performChessMove(source,target);
+                ChessPiece capturedPiece = match.performChessMove(source,target);
+            } catch (ChessException | InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
 
 
